d71d9ef7a81e47cab7fe3b432469db288fa56c04
funkycadet <ericsmithsonian2@gmail.com>
2023-01-23 23:33:16 +0100

    fix: resolve pycodestye error and fix pagination error

--- 0x00-pagination/0-simple_helper_function.py
+++ 0x00-pagination/0-simple_helper_function.py
@@ -4,8 +4,9 @@ with start index and end index corresponding
 to range of indexes to return in a list for
 a particular pagination parameters """
 
+
 def index_range(page, page_size):
     """ index_range function """
-    a = (page, page_size)
-    return a
-
+    start_index = (page - 1) * page_size
+    end_index = start_index + page_size
+    return (start_index, end_index)
