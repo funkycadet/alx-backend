b2a71a242adeadfaf1195b9a64869a96c30f411b
funkycadet <ericsmithsonian2@gmail.com>
2023-02-01 02:32:35 +0100

    task: basic babel setup

---
+++ 0x02-i18n/1-app.py
@@ -0,0 +1,31 @@
+#!/usr/bin/env python3
+""" Basic flask app that configures Babel """
+from flask import Flask, render_template
+from flask_babel import Babel
+
+
+class Config:
+    """ config class """
+    LANGUAGES = ["en", "fr"]
+
+    def __init__(self):
+        """ __init__ method """
+        self.locale = "en"
+        self.timezzone = "UTC"
+
+
+app = Flask(__name__)
+app.config.from_object(Config)
+
+
+babel = Babel(app)
+
+
+@app.route('/', strict_slashes=False)
+def root():
+    """ Homepage URL """
+    return render_template('1-index.html')
+
+
+if __name__ == '__main__':
+    app.run(host='127.0.0.1', port='5000')
