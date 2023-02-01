a2e9821b3f01f602be2fe3242fc98d3303f663e3
funkycadet <ericsmithsonian2@gmail.com>
2023-02-01 09:52:07 +0100

    fix: set locale and timezone for config class

--- 0x02-i18n/1-app.py
+++ 0x02-i18n/1-app.py
@@ -7,6 +7,8 @@ from flask_babel import Babel
 class Config:
     """ config class """
     LANGUAGES = ["en", "fr"]
+    BABEL_DEFAULT_LOCALE = "en"
+    BABEL_DEFAULT_TIMEZONE = "UTC"
 
     def __init__(self) -> None:
         """ __init__ method """
