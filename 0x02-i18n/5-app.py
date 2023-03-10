eaec7d073eea2dd3fc13117e0733985078d49217
funkycadet <ericsmithsonian2@gmail.com>
2023-03-10 01:35:55 +0100

    task: add module to handle a mock login system

---
+++ 0x02-i18n/5-app.py
@@ -0,0 +1,68 @@
+#!/usr/bin/env python3
+""" Basic flask app that configures Babel """
+from flask import Flask, render_template, request, g
+from flask_babel import Babel
+from typing import Union, Dict
+
+
+class Config:
+    """ config class """
+    LANGUAGES = ["en", "fr"]
+    BABEL_DEFAULT_LOCALE = "en"
+    BABEL_DEFAULT_TIMEZONE = "UTC"
+
+
+app = Flask(__name__)
+app.config.from_object(Config)
+babel = Babel(app)
+
+users = {
+    1: {"name": "Balou", "locale": "fr", "timezone": "Europe/Paris"},
+    2: {"name": "Beyonce", "locale": "en", "timezone": "US/Central"},
+    3: {"name": "Spock", "locale": "kg", "timezone": "Vulcan"},
+    4: {"name": "Teletubby", "locale": None, "timezone": "Europe/London"},
+}
+
+
+def get_user():
+    """ get_user method
+    Retrieves a user based on id
+    """
+    login_user = request.args.get('login_as', None)
+    if login_user is not None and int(login_user) in users.keys():
+        return users.get(int(login_user))
+    return None
+
+
+@app.before_request
+def before_request():
+    """ before request function
+    Function to perform some routines before each endpoints
+    """
+    user = get_user()
+    g.user = user
+
+
+@babel.localeselector
+def get_locale():
+    """ get_locale method
+    Set locale language from available languages """
+    data = request.query_string.decode('utf-8').split('&')
+    data_table = dict(map(
+        lambda i: (i if '=' in i else '{}='.format(i)).split('='),
+        data
+    ))
+    if 'locale' in data_table:
+        if data_table['locale'] in app.config["LANGUAGES"]:
+            return data_table['locale']
+    return request.accept_languages.best_match(app.config['LANGUAGES'])
+
+
+@app.route('/', strict_slashes=False)
+def root():
+    """ Homepage URL """
+    return render_template(('5-index.html'))
+
+
+if __name__ == '__main__':
+    app.run(host='127.0.0.1', port='5000')
